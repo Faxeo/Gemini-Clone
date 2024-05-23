adea41b7d0da2449d13e11a2ca95a9b8af4e912e
Faxeo <muhammadfazeel60@gmail.com>
2024-05-23 17:01:56 +0500

    Initial Commit

---
+++ src/context/context.jsx
@@ -0,0 +1,79 @@
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResults, setShowResults] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const delayPara = (index, nextWord) => {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord);
+        }, 75 * index);
+    };
+
+    const newChat = () => {
+        setLoading(false);
+        setShowResults(false);
+    };
+
+    const onSent = async (prompt) => {
+        setResultData("");
+        setLoading(true);
+        setShowResults(true);
+        let response = "";
+        if (prompt !== undefined) {
+            setRecentPrompt(prompt);
+            response = await runChat(prompt);
+        } else {
+            setPrevPrompts(prev => [...prev, input]);
+            setRecentPrompt(input);
+            response = await runChat(input);
+        }
+
+        let responseArray = response.split("**");
+        let newResponse = "";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            } else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+
+        let newResponse2 = newResponse.split("*").join("<br/>");
+        let newResponseArray = newResponse2.split(" ");
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord + " ");
+        }
+        setLoading(false); // Stop loading after processing the response
+    };
+
+    const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        input,
+        setInput,
+        showResults,
+        loading,
+        resultData,
+        newChat,
+    };
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    );
+};
+
+export default ContextProvider;
